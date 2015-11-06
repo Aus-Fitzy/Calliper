31bdc0fd214b3af799a938af9abebf7ef624238a
Dean Fitzpatrick <dean.fitzpatrick1@gmail.com>
2015-11-06 14:02:22 +1030

    Added calibration slider

--- app/src/main/java/com/example/dean/calliper/app/MeasureActivity.java
+++ app/src/main/java/com/example/dean/calliper/app/MeasureActivity.java
@@ -4,7 +4,6 @@ import android.bluetooth.BluetoothDevice;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.location.GpsStatus;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +11,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -32,6 +32,8 @@ public class MeasureActivity extends AppCompatActivity implements ServiceConnect
     private MetaWearBoard mwBoard;
     private BluetoothDevice btDevice;
     private Gpio gpioModule;
+    private int cal_max=1024;
+    private int cal_min=0;
 
     private final MetaWearBoard.ConnectionStateHandler connectionHandler= new MetaWearBoard.ConnectionStateHandler() {
         @Override
@@ -83,7 +85,6 @@ public class MeasureActivity extends AppCompatActivity implements ServiceConnect
                 if (isChecked) {
 
 
-
                     if (LEDModule != null) {
                         LEDModule.configureColorChannel(Led.ColorChannel.BLUE)
                                 .setRiseTime((short) 0).setPulseDuration((short) 1000)
@@ -100,6 +101,50 @@ public class MeasureActivity extends AppCompatActivity implements ServiceConnect
                 }
             }
         });
+
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar_max);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                cal_max = progress;
+                if( ((SeekBar) findViewById(R.id.seekBar_min)).getProgress() > cal_max){
+                    ((SeekBar) findViewById(R.id.seekBar_min)).setProgress(cal_max);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar_min);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                cal_min=progress;
+                if( ((SeekBar) findViewById(R.id.seekBar_max)).getProgress() < cal_min){
+                    ((SeekBar) findViewById(R.id.seekBar_max)).setProgress(cal_min);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
     }
     public void onMeasureClicked(View v) {
         short res;
@@ -114,6 +159,9 @@ public class MeasureActivity extends AppCompatActivity implements ServiceConnect
                 result.subscribe("pin0_adc", new RouteManager.MessageHandler() {
                     @Override
                     public void process(Message message) {
+                        //
+
+                        Short signal = message.getData(Short.class);
 
                         tv.setText(Short.toString(message.getData(Short.class)));
                         Log.i(TAG,"ADC" + message.getData(Short.class));
